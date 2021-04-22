a6daa85a78de95db47f1ead8a01990a79fc456b9
Aleksey Krylov <elsite2015@yandex.ru>
2021-04-22 08:55:48 +0400

    1. Spring ORM [#477077]

--- src/main/java/ru/job4j/accident/model/Accident.java
+++ src/main/java/ru/job4j/accident/model/Accident.java
@@ -1,24 +1,16 @@
 package ru.job4j.accident.model;
 
+import javax.persistence.*;
 import java.util.Objects;
-import java.util.Set;
 
+@Entity
+@Table(name = "accident")
 public class Accident {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name;
-    private String text;
-    private String address;
-    private AccidentType type;
-    private Set<Rule> rules;
 
-    public static Accident of(int id, String name, String text, String address) {
-        Accident accident = new Accident();
-        accident.id = id;
-        accident.name = name;
-        accident.text = text;
-        accident.address = address;
-        return accident;
-    }
+    private String name;
 
     public int getId() {
         return id;
@@ -36,46 +28,10 @@ public class Accident {
         this.name = name;
     }
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public AccidentType getType() {
-        return type;
-    }
-
-    public void setType(AccidentType type) {
-        this.type = type;
-    }
-
-    public Set<Rule> getRules() {
-        return rules;
-    }
-
-    public void setRules(Set<Rule> rules) {
-        this.rules = rules;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Accident accident = (Accident) o;
         return id == accident.id;
     }
@@ -83,11 +39,5 @@ public class Accident {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Accident{" + "id=" + id + ", name='" + name + '\'' + ", text='" + text + '\''
-                + ", address='" + address + '\'' + ", type=" + type + '}';
     }
 }
